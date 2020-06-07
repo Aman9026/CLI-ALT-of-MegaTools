6eca2bee0989ec73a43747fd98ef15555f1606c6
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-06-07 12:10:03 +0530

    Update meg.sh

--- meg.sh
+++ meg.sh
@@ -20,3 +20,19 @@ json_data=`curl --silent --request POST --data-binary '[{"a":"g","g":1,"p":"'$id
 # get the download url
 #Formerly $12
 new_url=`echo $json_data | awk -F '"' '{print $14}'`
+
+
+# get the file name, have to do a lot of weird things because openssl is tricky
+tmp=`echo $json_data | awk -F '"' '{print $6}' | sed -e 's/-/+/g' -e 's/_/\//g' -e 's/,//g' | base64 --decode --ignore-garbage 2> /dev/null | xxd -p | tr -d '\n' > enc_attr.mdtmp`
+tmp=`xxd -p -r enc_attr.mdtmp > enc_attr2.mdtmp`
+openssl enc -d -aes-128-cbc -K $key -iv 0 -nopad -in enc_attr2.mdtmp -out dec_attr.mdtmp
+#Changed this too, can't remember where from
+file_name=`cat dec_attr.mdtmp | awk -F '"' '{print $8}'`
+rm -f *.mdtmp
+
+# download the file and decrypt it
+enc_file=${file_name}.enc
+
+curl --output $enc_file $new_url
+openssl enc -d -aes-128-ctr -K $key -iv $iv -in $enc_file -out $file_name
+rm -f $enc_file
